074c86d00c002f764fd44855f274251920eb5dd2
AlgoWebBot <shimulzahan636@gmail.com>
2023-12-11 17:40:55 +0600

    skills parts updated

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -11,6 +11,8 @@ import { SiExpress } from "react-icons/si";
 import { DiMongodb } from "react-icons/di";
 import { SiJsonwebtokens } from "react-icons/si";
 import { IoLogoFirebase } from "react-icons/io5";
+import { SiMui } from "react-icons/si";
+import { IoLogoVercel } from "react-icons/io5";
 
 const Skills = () => {
     return (
@@ -22,70 +24,101 @@ const Skills = () => {
 
             <div className='pt-10 px-5 text-center'>
                 <div className='text-lg text-white w-[80%] mx-auto mb-10'>I'm Versatile MERN Stack Developer | Proficient in HTML, CSS, Tailwind CSS, React, and JavaScript for Frontend | Comfortable with Backend Technologies: Node.js, Express.js, MongoDB | Experienced in Firebase for Deployment | Eager to Contribute and Learn in a Dynamic Development Environment</div>
-                <div className='flex justify-center items-center gap-10 flex-wrap'>
-                    <div>
-                        <div className="radial-progress" style={{ "--value": "70", color: '#20c997', "--size": "7rem", "--thickness": "0.7rem" }} role="progressbar">
-                            <TiHtml5 className='text-5xl text-yellow-600' />
-                        </div>
-                        <h1 className='text-white text-xl'>70%<br /></h1>
-                    </div>
-                    <div>
-                        <div className="radial-progress" style={{ "--value": "65", color: '#20c997', "--size": "7rem", "--thickness": "0.7rem" }} role="progressbar">
-                            <FaCss3 className='text-5xl text-blue-500' />
-                        </div>
-                        <h1 className='text-white text-xl'>65%<br /></h1>
-                    </div>
-                    <div>
-                        <div className="radial-progress" style={{ "--value": "80", color: '#20c997', "--size": "7rem", "--thickness": "0.7rem" }} role="progressbar">
-                            <SiTailwindcss className='text-5xl text-[#3abdf8]' />
-                        </div>
-                        <h1 className='text-white text-xl'>80%<br /></h1>
-                    </div>
-                    <div>
-                        <div className="radial-progress" style={{ "--value": "70", color: '#20c997', "--size": "7rem", "--thickness": "0.7rem" }} role="progressbar">
-                            <IoLogoJavascript className='text-5xl text-yellow-500' />
-                        </div>
-                        <h1 className='text-white text-xl'>70%<br /></h1>
-                    </div>
-                    <div>
-                        <div className="radial-progress" style={{ "--value": "80", color: '#20c997', "--size": "7rem", "--thickness": "0.7rem" }} role="progressbar">
-                            <FaReact className='text-5xl text-[#3abdf8]' />
-                        </div>
-                        <h1 className='text-white text-xl'>80%<br /></h1>
-                    </div>
-                </div>
 
-
-                <div className='flex justify-center items-center gap-10 flex-wrap mt-10'>
-                    <div>
-                        <div className="radial-progress" style={{ "--value": "65", color: '#20c997', "--size": "7rem", "--thickness": "0.7rem" }} role="progressbar">
-                            <SiExpress className='text-5xl text-[#ffff]' />
+                <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-16 px-10">
+                    <div className="text-start space-y-3">
+                        <h1 className='text-white font-bold text-2xl'>Frontend</h1>
+                        <div>
+                            <div className='flex justify-between'>
+                                <TiHtml5 className='text-2xl text-yellow-600' />
+                                <h1 className='text-white'>70%</h1>
+                            </div>
+                            <progress className="progress progress-success bg-yellow-600 w-full" color='#20c997' value="70" max="100"></progress>
                         </div>
-                        <h1 className='text-white text-xl'>65%<br /></h1>
+                        <div>
+                            <div className='flex justify-between'>
+                                <FaCss3 className='text-2xl text-blue-500' />
+                                <h1 className='text-white'>65%</h1>
+                            </div>
+                            <progress className="progress progress-success bg-blue-500 w-full" color='#20c997' value="65" max="100"></progress>
+                        </div>
+                        <div>
+                            <div className='flex justify-between'>
+                                <SiTailwindcss className='text-2xl text-[#3abdf8]' />
+                                <h1 className='text-white'>70%</h1>
+                            </div>
+                            <progress className="progress progress-success bg-[#3abdf8] w-full" color='#3abdf8' value="70" max="100"></progress>
+                        </div><div>
+                            <div className='flex justify-between'>
+                                <IoLogoJavascript className='text-2xl text-yellow-500' />
+                                <h1 className='text-white'>70%</h1>
+                            </div>
+                            <progress className="progress progress-success bg-yellow-500 w-full" color='#20c997' value="70" max="100"></progress>
+                        </div><div>
+                            <div className='flex justify-between'>
+                                <FaReact className='text-2xl text-[#3abdf8]' />
+                                <h1 className='text-white'>80%</h1>
+                            </div>
+                            <progress className="progress progress-success bg-[#3abdf8] w-full" color='#20c997' value="80" max="100"></progress>
+                        </div>
                     </div>
-                    <div>
-                        <div className="radial-progress" style={{ "--value": "50", color: '#20c997', "--size": "7rem", "--thickness": "0.7rem" }} role="progressbar">
-                            <IoLogoNodejs className='text-5xl text-[#58a846]' />
+                    <div className="">
+                        <div className="text-start space-y-3">
+                            <h1 className='text-white font-bold text-2xl'>Backend</h1>
+                            <div>
+                                <div className='flex justify-between'>
+                                    <SiExpress className='text-2xl text-[#ffff]' />
+                                    <h1 className='text-white'>70%</h1>
+                                </div>
+                                <progress className="progress progress-success bg-white w-full" color='#20c997' value="70" max="100"></progress>
+                            </div>
+                            <div>
+                                <div className='flex justify-between'>
+                                    <IoLogoNodejs className='text-2xl text-[#58a846]' />
+                                    <h1 className='text-white'>65%</h1>
+                                </div>
+                                <progress className="progress progress-success bg-white w-full" color='#20c997' value="65" max="100"></progress>
+                            </div>
+                            <div>
+                                <div className='flex justify-between'>
+                                    <DiMongodb className='text-2xl text-green-500' />
+                                    <h1 className='text-white'>70%</h1>
+                                </div>
+                                <progress className="progress progress-success bg-white w-full" color='#20c997' value="70" max="100"></progress>
+                            </div><div>
+                                <div className='flex justify-between'>
+                                    <SiJsonwebtokens className='text-2xl text-[#dc3bfe]' />
+                                    <h1 className='text-white'>70%</h1>
+                                </div>
+                                <progress className="progress progress-success bg-white w-full" color='#20c997' value="70" max="100"></progress>
+                            </div>
                         </div>
-                        <h1 className='text-white text-xl'>55%<br /></h1>
                     </div>
-                    <div>
-                        <div className="radial-progress" style={{ "--value": "60", color: '#20c997', "--size": "7rem", "--thickness": "0.7rem" }} role="progressbar">
-                            <DiMongodb className='text-5xl text-green-500' />
+                    <div className="">
+                        <div className="text-start space-y-3">
+                            <h1 className='text-white font-bold text-2xl'>Web Tools</h1>
+                            <div>
+                                <div className='flex justify-between'>
+                                    <IoLogoFirebase className='text-2xl text-[#ffcb2b]' />
+                                    <h1 className='text-white'>80%</h1>
+                                </div>
+                                <progress className="progress progress-success bg-white w-full" color='#ffcb2b' value="80" max="100"></progress>
+                            </div>
+                            <div>
+                                <div className='flex justify-between'>
+                                    <SiMui className='text-2xl text-[#3abdf8]' />
+                                    <h1 className='text-white'>80%</h1>
+                                </div>
+                                <progress className="progress progress-success bg-white w-full" color='#3abdf8' value="80" max="100"></progress>
+                            </div>
+                            <div>
+                                <div className='flex justify-between'>
+                                    <IoLogoVercel className='text-2xl text-[#000]' />
+                                    <h1 className='text-white'>70%</h1>
+                                </div>
+                                <progress className="progress progress-success bg-white w-full" color='#000' value="70" max="100"></progress>
+                            </div>
                         </div>
-                        <h1 className='text-white text-xl'>60%<br /></h1>
-                    </div>
-                    <div>
-                        <div className="radial-progress" style={{ "--value": "60", color: '#20c997', "--size": "7rem", "--thickness": "0.7rem" }} role="progressbar">
-                            <IoLogoFirebase className='text-5xl text-[#ffcb2b]' />
-                        </div>
-                        <h1 className='text-white text-xl'>50%<br /></h1>
-                    </div>
-                    <div>
-                        <div className="radial-progress" style={{ "--value": "70", color: '#20c997', "--size": "7rem", "--thickness": "0.7rem" }} role="progressbar">
-                            <SiJsonwebtokens className='text-5xl text-[#dc3bfe]' />
-                        </div>
-                        <h1 className='text-white text-xl'>70%<br /></h1>
                     </div>
                 </div>
             </div>
